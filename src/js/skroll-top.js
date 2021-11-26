cf579300e33f97c25db08787380623f4d5e16b40
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2021-11-26 21:38:12 +0200

    raketa-js

--- src/js/skroll-top.js
+++ src/js/skroll-top.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $("#raketa").on("click","a", function (event) {
+    $("#raketa-js").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
+        $('body,html').animate({scrollTop: top}, 1300);
     });
 });
